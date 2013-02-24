5f7501037c609cce49e04895927c6c891045a7cd
Luis Lavena <luislavena@gmail.com>
2013-02-24 02:52:14 -0300

    Add mysql-server and default root password

--- chef/roles/db.rb
+++ chef/roles/db.rb
@@ -2,5 +2,14 @@ name "db"
 description "A database role"
 
 run_list(
-  "role[base]"
+  "role[base]",
+  "recipe[mysql::server]"
+)
+
+default_attributes(
+  "mysql" => {
+    "server_root_password" => "iloverandompasswordsbutthiswilldo",
+    "server_repl_password" => "iloverandompasswordsbutthiswilldo",
+    "server_debian_password" => "iloverandompasswordsbutthiswilldo"
+  }
 )
